98e2f9a4b8afe7a510f90c08bcc4f9c643acfe85
danisjiang <danisjiang@gmail.com>
2020-11-09 11:10:02 +0800

    2.2 update login result and some status prompt

--- client.py
+++ client.py
@@ -52,9 +52,6 @@ class LoginWindow(QtWidgets.QDialog):
         app.exec_()
 
     def login(self):
-        loginStatus = QtWidgets.QMessageBox()
-        loginStatus.setText("login success")
-        loginStatus.exec_()
         self.close()
 
 
@@ -96,6 +93,7 @@ class Client(QtWidgets.QWidget):
         用户登陆
         '''
         name = self.loginWindow.username.text()
+        self.setWindowTitle(name + " - Python-ChatRoom")
         pwd = self.loginWindow.passwd.text()
         self.auth = {
             "name": name,
@@ -108,7 +106,15 @@ class Client(QtWidgets.QWidget):
         msg = json.dumps(msg)
         msg = bytes(msg, encoding='utf-8')
         self.sock.sendto(msg, self.serverAddr)
-
+        msg, addr = self.sock.recvfrom(8192)
+        msg = str(msg, encoding="utf-8")
+        loginStatus = QtWidgets.QMessageBox()
+        loginStatus.setText(msg)
+        loginStatus.exec_()
+        if msg == "Login failed":
+            # self.loginWindow = LoginWindow()
+            # self.start()
+            sys.exit()
         self.setWindowFlag(QtCore.Qt.WindowStaysOnTopHint)
         self.show()
 
@@ -116,20 +122,21 @@ class Client(QtWidgets.QWidget):
         '''
         接收消息
         '''
-        self.queue.append(msg)
-        self.chatWindow()
+        # self.queue.append(msg)
+        # self.chatWindow()
+        self.mainEdit.append(f'{msg} \n')
 
-    def chatWindow(self):
-        '''
-        聊天界面
-        '''
-        self.mainEdit.clear()
-        if len(self.queue)>=6:
-            for msg in self.queue[-6:]:
-                self.mainEdit.append(f'{msg} \n')
-        else:
-            for msg in self.queue:
-                self.mainEdit.append(f'{msg} \n')
+    # def chatWindow(self):
+    #     '''
+    #     聊天界面
+    #     '''
+    #     self.mainEdit.clear()
+    #     if len(self.queue)>=6:
+    #         for msg in self.queue[-6:]:
+    #             self.mainEdit.append(f'{msg} \n')
+    #     else:
+    #         for msg in self.queue:
+    #             self.mainEdit.append(f'{msg} \n')
 
 
     def pack(self):
@@ -148,10 +155,22 @@ class Client(QtWidgets.QWidget):
                     msg["toWho"] = who
                 else:
                     msg["type"] = ""
-                msg["text"] = self.inputEdit.toPlainText()
+                if self.inputEdit.toPlainText() == "":
+                    msg["type"] = ""
+                    warning = QtWidgets.QMessageBox()
+                    warning.setText("say something")
+                    warning.exec_()
+                else:
+                    msg["text"] = self.inputEdit.toPlainText()
             elif mode == "broadcast":
                 msg["type"] = "broadcast"
-                msg["text"] = self.inputEdit.toPlainText()
+                if self.inputEdit.toPlainText() == "":
+                    msg["type"] = ""
+                    warning = QtWidgets.QMessageBox()
+                    warning.setText("say something")
+                    warning.exec_()
+                else:
+                    msg["text"] = self.inputEdit.toPlainText()
             elif mode == "list all users":
                 msg["type"] = "show"
         msg = json.dumps(msg)
@@ -167,11 +186,11 @@ class Client(QtWidgets.QWidget):
         # t = threading.Thread(target=self.recieve,args=(self.addr,))
         # t.setDaemon(True)
         # t.start()
+
+        self.login()
         self.recvThread = RecvThread(self.sock)
         self.recvThread.signal.connect(self.recieve)
         self.recvThread.start()
-
-        self.login()
         self.sendButton.clicked.connect(self.send)
 
 
